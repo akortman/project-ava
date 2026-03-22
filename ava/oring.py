c7fd88e7a6942e1bf31cbe11b0358d7b64774d58
April Kortman <april@rodent.zone>
2026-03-22 13:43:14 +1030

    chore: cleanup

--- ava/oring.py
+++
@@ -1,6 +0,0 @@
-import cadquery as cq
-from ocp_vscode import show
-
-result = cq.Workplane(origin=(20, 0, 0)).circle(2).revolve(180, (-20, 0, 0), (-20, -1, 0))
-result2 = cq.Workplane("XZ", origin=(20, 0, 0)).circle(2).revolve(180, (-20, 0, 0), (-20, -1, 0))
-show(result2)
